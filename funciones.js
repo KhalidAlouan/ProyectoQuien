97f97c89e8bc4f04d53b53dfb323bca675d6b416
sergi <scomacorcuera@iesesteveterradas.cat>
2018-10-19 20:13:14 +0200

    Subiendo espect 17

--- funciones.js
+++ funciones.js
@@ -1,17 +1,51 @@
+//GLOBAL VAR
+var cartas = 11;
+var cartaFinal;
+//GLOBAL END
 
+function endGame(){
+	cartaServidor = document.getElementById("servCard");
+	cartaServidorImg = cartaServidor.children[1].children[0];
+	var cartas = document.getElementsByClassName("card");
+	for (var i = 0 ; cartas.length -1 >= i; i++) {
+
+		if (cartas[i].classList.contains("flipped") == false) {
+			cartaFinal = cartas[i].children[0].children[0];
+		}
+	}
+	if (cartaServidorImg.src == cartaFinal.src ) {
+		console.log("Win");
+	}else{
+		console.log("Pringado");
+	}
+
+	
+}
 //Se encarga de añadir la class que permite el efecto de girar.
 function flip(element) {
-	cartaServidor = document.getElementById("servCard").id;
-	element = element.target.parentNode.parentNode;
-	if (element.id != cartaServidor) {
-    	element.classList.add("flipped");
-	}  
+	if (cartas!=0) {
+		cartaServidor = document.getElementById("servCard").id;
+		element = element.target.parentNode.parentNode;
+		if(element.classList.contains("flipped")== false && element.id != cartaServidor ){
+			cartas--;
+		}
+		if (element.id != cartaServidor) {
+	    	element.classList.add("flipped");
+
+		}
+	}
+	if (cartas == 0){
+		endGame();
+	}
+	
+	
 
 }
 //Funcion que se ejecuta nada mas cagar la pagina que añade la funcion flip() a las cartas.
 window.onload = function addEvent(){
 	var cartas = document.getElementsByClassName("card");
 	for (var i = 0 ; cartas.length -1 >= i; i++) {
+
 		cartas[i].addEventListener("click",flip);
 	}
 }
